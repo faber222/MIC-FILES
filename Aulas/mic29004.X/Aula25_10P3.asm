9d9d9eb9a307fb77903c6d4b65bc47d3e7dff67a
faber222 <faberbernardo78@gmail.com!>
2022-11-01 11:34:24 -0300

    finalizado

--- Aulas/mic29004.X/Aula25_10P3.asm
+++ Aulas/mic29004.X/Aula25_10P3.asm
@@ -1,25 +1,72 @@
-;DEFINI�C~OES
-.equ LED = PD2 ;LED �e o substituto de PD2 na programa�c~ao
-.equ BOTAO = PD7 ;BOTAO �e o substituto de PD7 na programa�c~ao
+;DEFINIÇÕES
+.equ LED7 = PD7 ;LED 7 o substituto de PD2 na programação
+.equ LED6 = PD6 ;LED 6 o substituto de PD2 na programação
+.equ LED5 = PD5 ;LED 5 o substituto de PD2 na programação
+.equ LED4 = PD4 ;LED 4 o substituto de PD2 na programação
+.equ LED3 = PD3 ;LED 3 o substituto de PD2 na programação
+.equ LED2 = PD2 ;LED 2 o substituto de PD2 na programação
+.equ LED1 = PD1 ;LED 1 o substituto de PD2 na programação
+.equ LED0 = PD0 ;LED 0 o substituto de PD2 na programação
+.equ BOTAOSELECAO = PB1 ;BOTAO � o substituto de PD7 na programação
+.equ BOTAOAJUSTE = PB0 ;BOTAO � o substituto de PD7 na programação
 .def AUX = R16 ;R16 tem agora o nome de AUX
 
 setup:
-LDI AUX,0b00000100 ;carrega AUX com o valor 0x04 (1 = sa�?da e 0 = entrada)
-OUT DDRD,AUX ;configura PORTD, PD2 sa�?da e demais pinos entradas
-LDI AUX,0b11111111 ;habilita o pull-up para o bot~ao e apaga o LED
-OUT PORTD,AUX
+LDI AUX,0b11111111 
+OUT DDRD,AUX 
+OUT PORTD,AUX 
+OUT PORTB,AUX
 
 ;----------------------
-;LA�CO PRINCIPAL
+;LAÇO PRINCIPAL
 ;----------------------
-naoPress: ;loop bot~ao n~ao pressionado (pull-up)
-sbi PORTD,LED ;desliga LED
-sbic PIND,BOTAO ;verifica se o bot~ao foi pressionado, sen~ao
-rjmp naoPress ;volta e fica preso no la�co naoPress
+naoPress: ;loop botao nao pressionado (pull-up)
+sbis PINB,BOTAOSELECAO ;verifica se o botao nao foi pressionado, senao
+rjmp pressSelecao
 
-press: ;loop bot~ao pressionado
-cbi PORTD,LED ;desliga LED
-sbis PIND,BOTAO ;verifica se o bot~ao foi solto, sen~ao
-rjmp press ;sen~ao, aguarda.
+rjmp desligaLedSelecao ;volta e fica preso no la�co naoPress
 
-rjmp naoPress ;vai para o loop do "bot~ao pressionado"
+;--------------------------------------;
+pressSelecao: ;loop botao pressionado
+sbis PINB,BOTAOSELECAO ;verifica se o botao nao foi solto, senao
+rjmp ligaLedSelecao ;senao, aguarda.
+
+rjmp desligaLedSelecao
+
+;--------------------------------------;
+pressAjuste: ;loop botao pressionado
+sbis PINB,BOTAOAJUSTE ;verifica se o botao nao foi solto, senao
+rjmp ligaLedAjuste
+
+rjmp desligaLedAjuste
+
+;---------------------------------------;
+desligaLedAjuste:
+cbi PORTD,LED3 ;desliga LED3
+cbi PORTD,LED2 ;desliga LED2
+cbi PORTD,LED1 ;desliga LED1
+cbi PORTD,LED0 ;desliga LED0
+rjmp pressSelecao
+    
+desligaLedSelecao:
+cbi PORTD,LED7 ;desliga LED7
+cbi PORTD,LED6 ;desliga LED6
+cbi PORTD,LED5 ;desliga LED5
+cbi PORTD,LED4 ;desliga LED4
+rjmp pressAjuste
+    
+ligaLedAjuste:
+sbi PORTD,LED3 ;liga LED3
+sbi PORTD,LED2 ;liga LED2
+sbi PORTD,LED1 ;liga LED1
+sbi PORTD,LED0 ;liga LED0
+
+rjmp pressSelecao
+    
+ligaLedSelecao:
+sbi PORTD,LED7 ;liga LED7
+sbi PORTD,LED6 ;liga LED6
+sbi PORTD,LED5 ;liga LED5
+sbi PORTD,LED4 ;liga LED4
+    
+rjmp pressAjuste
