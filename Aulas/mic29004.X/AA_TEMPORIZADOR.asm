da06346864d18ecf759c5e48c71354c144f81144
faber222 <faberbernardo78@gmail.com>
2022-11-29 11:31:08 -0300

    Tarefa Sobre temporizador

---
+++ Aulas/mic29004.X/AA_TEMPORIZADOR.asm
@@ -0,0 +1,88 @@
+.INCLUDE <m328Pdef.inc>
+    
+; Fiz baseado na AA anterior, que era sobre interrupção
+; Onde devemos fazer com que o led0 pisque a cada 2s 
+; E quando pressionado o botão PD3, deveria ligar o led1
+; E quando pressionado o botão PD2, deveria desligar o led1
+
+;DEFINICOES
+.equ BOTAO = PD3
+.equ BOTAO2 = PD2
+.equ L0 = PB0
+.equ L1 = PB1
+.def COUNT = R16
+
+.ORG 0x0000 ; Reset vector
+    RJMP setup
+
+.ORG 0x0002 ; Vetor (endereco na Flash) da INT1
+    RJMP isr_int0
+
+.ORG 0x0004 ; Vetor (endereco na Flash) da INT1
+    RJMP isr_int1    
+
+.ORG 0x0020
+    RJMP isr_tc0b
+    
+.ORG 0x0034 ; primeira end. livre depois dos vetores
+setup:
+    ldi COUNT,0x03 ; 0b00000011
+    out DDRB,COUNT ; configura PB3/2 como saida
+    out PORTB,COUNT ; desliga os LEDs
+    cbi DDRD, BOTAO ; configura o PD3 como entrada
+    sbi PORTD, BOTAO ; liga o pull-up do PD
+    cbi DDRD, BOTAO2 ; configura o PD2 como entrada
+    sbi PORTD, BOTAO2 ; liga o pull-up do PD2
+
+    LDI COUNT, 0b00000010 ;
+    STS EICRA, COUNT ; config. INT0 sensivel a borda
+    SBI EIMSK, INT0 ; habilita a INT0
+    
+    LDI COUNT, 0b00001000 ;
+    STS EICRA, COUNT ; config. INT1 sensivel a borda
+    SBI EIMSK, INT1 ; habilita a INT1
+    
+    LDI COUNT, 0b00000101   ;TC0 com prescaler de 1024, a 16 MHz gera
+    OUT TCCR0B, COUNT	    ; uma interrupcaoo a cada 16,384 ms
+    LDI COUNT, 1
+    STS TIMSK0, COUNT	    ; habilita int. do TC0B (TIMSK0(0) =TOIE0 <- 1)
+
+    SEI ; habilita a interrupcao global ...
+; ... (bit I do SREG)
+    
+main:
+    rjmp main
+
+;-------------------------------------------------
+; Rotina de Interrupcao (ISR) da INT0
+;-------------------------------------------------
+isr_int1:
+    push COUNT ; Salva o contexto (SREG)
+    in COUNT, SREG
+    push COUNT
+    sbi PORTB,L1 ; desliga L1
+    pop COUNT ; Restaura o contexto (SREG)
+    out SREG,COUNT
+    pop COUNT
+    reti ; retorna da interrupcao
+
+
+isr_int0:
+    push COUNT ; Salva o contexto (SREG)
+    in COUNT, SREG
+    push COUNT
+    cbi PORTB,L1 ; liga L1
+    pop COUNT ; Restaura o contexto (SREG)
+    out SREG,COUNT
+    pop COUNT
+    reti ; retorna da interrupcao
+
+isr_tc0b:
+    inc COUNT	    ; incrementa o contador
+    cpi COUNT, 62   ; verifica se é 62
+    brne fim	    ; se não for igual, salta para o fim
+    sbi PINB, L0    ; inverte o estado do LED depois de entrar 62 vezes
+		    ; na interrupcao: 62 * 16,384 ms = 1.015,81ms
+    ldi COUNT,0
+fim:
+    reti
